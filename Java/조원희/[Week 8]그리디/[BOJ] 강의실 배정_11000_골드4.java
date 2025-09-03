3e568b442d7d1036923a7c817fcfe7831e13d736
woni-jo <woni0517@naver.com>
2025-09-03 16:57:03 +0900

    [BOG] 조원희 / 강의실 배정 / 골드4 / 35

---
+++ Java/조원희/[Week 8]그리디/[BOJ] 강의실 배정_11000_골드4.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N =  Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+        StringTokenizer st;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if(o1[0] == o2[0]) {
+                return  o1[1] - o2[1];
+            }
+            return o1[0] - o2[0];
+        });
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < N; i++) {
+            if(!pq.isEmpty() && pq.peek() <= arr[i][0]) {
+                pq.poll();
+            }
+            pq.add(arr[i][1]);
+        }
+
+        System.out.println(pq.size());
+        br.close();
+    }
+}
